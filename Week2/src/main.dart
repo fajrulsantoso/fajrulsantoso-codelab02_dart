25c7a305f4adfaf9b512e6f6c74fc4b6018381ce
Fajrul Santoso <fajrulsantoso10@gmail.com>
2025-09-18 01:13:48 +0700

    Pebaiki kode yang rusak

--- Week2/src/main.dart
+++ Week2/src/main.dart
@@ -1,5 +1,5 @@
 void main() {
   for (int i = 0; i < 10; i++) {
-    print('Nama saya adalah fajrulsantpsp, sekarang berumur ${20 - i}');
+    print('Nama saya adalah fajrulsantoso, sekarang berumur ${20 - i}');
   }
 }
