246d8a8494e798a39d59d00954c8e22ceec7fc12
Fajrul Santoso <fajrulsantoso10@gmail.com>
2025-09-17 22:27:46 +0700

    Pebaiki kode yang rusak

--- Week3/src/Praktikum3.dart
+++ Week3/src/Praktikum3.dart
@@ -1,17 +1,9 @@
-// for (Index = 10; index < 27; index) {
-//   print(Index);
-// }
-// If (Index == 21) break;
-// Else If (index > 1 || index < 7) continue;
-// print(index);
-
-// karena menggunakan index maka harus mendeklarasikan index
 void main() {
   for (int index = 10; index < 27; index++) {
     if (index == 21)
-      break;
-    else if (index > 1 || index < 7)
-      continue;
+      break; // stop kalau sudah 21
+    else if (index > 1 && index < 7)
+      continue; // lewati angka 2–6 (tapi ini gak kepakai karena mulai dari 10)
     print(index);
   }
 }
