6fc74d4fb772dfbfc0e2129e7d3a0bc43992fe22
Fajrul Santoso <fajrulsantoso10@gmail.com>
2025-11-12 19:04:41 +0700

    Perbaiki eror

--- week10/master_plan/lib/models/plan.dart
+++ week10/master_plan/lib/models/plan.dart
@@ -7,15 +7,13 @@ class Plan {
   const Plan({this.name = '', this.tasks = const []});
 
   Plan copyWith({String? name, List<Task>? tasks}) {
-    return Plan(name: name ?? this.name, tasks: tasks ?? this.tasks);
+    return Plan(
+      name: name ?? this.name,
+      tasks: tasks ?? this.tasks,
+    );
   }
 
   int get completedCount => tasks.where((task) => task.complete).length;
 
-  String get completenessMessage =>
-      '$completedCount dari ${tasks.length} tugas selesai';
-
-  // Untuk tampilan di list
-  String get shortCompletenessMessage =>
-      '$completedCount out of ${tasks.length} tasks';
+  String get completenessMessage => '$completedCount dari ${tasks.length} tugas selesai';
 }
