d14a897195882cc4cce98f01e56dd2561822ab97
Fajrul Santoso <fajrulsantoso10@gmail.com>
2025-11-20 12:47:59 +0700

    W13: Jawaban Soal 2

--- week13/store_data_fajrulking/lib/main.dart
+++ week13/store_data_fajrulking/lib/main.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
+import 'dart:convert'; // Optional, jika ingin decode JSON menjadi Map/List
 
 void main() {
   runApp(const MyApp());
 }
 
+// 💻 Langkah 6: Buat class MyApp
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -18,6 +19,7 @@ class MyApp extends StatelessWidget {
   }
 }
 
+// 💻 Langkah 6: Buat StatefulWidget MyHomePage
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
 
@@ -25,12 +27,41 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
+// 💻 Langkah 6: Tambahkan _MyHomePageState
 class _MyHomePageState extends State<MyHomePage> {
+  // 💻 Langkah 6: Tambahkan variabel pizzaString
+  String pizzaString = '';
+
+  // 🚀 Langkah 8: Panggil readJsonFile di initState
+  @override
+  void initState() {
+    super.initState();
+    readJsonFile(); // Memanggil method untuk membaca JSON saat widget dibuat
+  }
+
+  // 💾 Langkah 7: Tambahkan Method readJsonFile
+  Future<void> readJsonFile() async {
+    String myString = await DefaultAssetBundle.of(
+      context,
+    ).loadString('assets/pizzalist.json');
+    setState(() {
+      pizzaString = myString;
+    });
+  }
+
+  // 💻 Langkah 9: Tampilkan hasil JSON di body
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Flutter JSON FAJRUL SANTOSO')),
-      body: Container(),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: pizzaString.isEmpty
+            ? const Center(child: CircularProgressIndicator())
+            : SingleChildScrollView(
+                child: Text(pizzaString, style: const TextStyle(fontSize: 16)),
+              ),
+      ),
     );
   }
 }
